73711f84ad039d7cf7c0c029c61e43ae34117755
Kongnyuy Livingston <99125408+Livingstonkongnyuy@users.noreply.github.com>
2023-06-01 04:33:00 -0700

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type

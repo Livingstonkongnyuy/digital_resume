1ef9c372c787b1bf2d8e4decc748a120687242a3
Kongnyuy Livingston <99125408+Livingstonkongnyuy@users.noreply.github.com>
2023-06-03 20:47:46 -0700

    Update settings.py for static not rendereing

--- resume/settings.py
+++ resume/settings.py
@@ -59,10 +59,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'resume.urls'
 
@@ -131,14 +131,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -147,12 +149,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # settings.py
-
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kongnyuylivingstone@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bamenda01'
-DEFAULT_FROM_EMAIL = 'kongnyuylivingstone@gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'kongnyuylivingstone@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Bamenda01'
+# DEFAULT_FROM_EMAIL = 'kongnyuylivingstone@gmail.com'
